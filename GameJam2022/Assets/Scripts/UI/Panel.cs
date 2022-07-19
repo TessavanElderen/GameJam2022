e53b66e875a8abb36b2de4b01e9bbc8222ba9c72
Tessa van Elderen <tessa.vanelderen@gmail.com>
2022-07-19 18:25:36 +0200

    Update panel 2

--- GameJam2022/Assets/Scripts/UI/Panel.cs
+++ GameJam2022/Assets/Scripts/UI/Panel.cs
@@ -4,24 +4,5 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Panel : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private Image myPanel;
-
-    [SerializeField] Color whiteColor, blackColor;
-    Color currentColor;
-    Image panelImage;
-
-    private void Start()
-    {
-        panelImage = GetComponent<Image>();
-        panelImage.material.color = whiteColor;
-        currentColor = whiteColor; 
-    }
-    void Update()
-    {
-        if (player.gameObject.name == "Door")
-        {
-            panelImage.material.color = Color.Lerp(panelImage.material.color, currentColor, 0.1f);
-        }
-    }
+    
 }
