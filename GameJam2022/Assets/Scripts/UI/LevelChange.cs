21a3694eb4f96ce01e61f7f65da757c0a8de7e98
Tessa van Elderen <tessa.vanelderen@gmail.com>
2022-07-20 11:26:04 +0200

    Panel fade af.

--- GameJam2022/Assets/Scripts/UI/LevelChange.cs
+++ GameJam2022/Assets/Scripts/UI/LevelChange.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LevelChange : MonoBehaviour
 {
-
     [SerializeField] private Animator animator;
-
     [SerializeField] private int levelToLoad; 
 
+    //This is a trigger to go to the next scene
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
@@ -17,6 +16,7 @@ public class LevelChange : MonoBehaviour
         }
     }
 
+    // The fade to next scene 
     public void FadeToLevel(int levelIndex)
     {
         levelToLoad = levelIndex;
@@ -24,6 +24,7 @@ public class LevelChange : MonoBehaviour
 
     }
 
+    //Going to the next scene. 
     public void OnFadeComplete()
     {
         SceneManager.LoadScene(levelToLoad);
