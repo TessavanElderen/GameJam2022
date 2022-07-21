8329911fd8e6cc3ded3862558be5e3b9185cf040
Tessa van Elderen <tessa.vanelderen@gmail.com>
2022-07-21 16:49:20 +0200

    Coin Ui af

--- GameJam2022/Assets/Scripts/Player/CoinCounter.cs
+++ GameJam2022/Assets/Scripts/Player/CoinCounter.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 using TMPro;
 public class CoinCounter : MonoBehaviour
 {
+    [Header("Coin Count")]
     [SerializeField] public int coinCount;
     [SerializeField] public int specialCoinCount;
 
+    [Header("Coin Text")]
     [SerializeField] TMP_Text textCoin; 
     [SerializeField] TMP_Text textSpecialCoin;
 
+    [Header("Black Canvas Coin Text")]
     [SerializeField] TMP_Text blackCanvasCoin; 
-    [SerializeField] TMP_Text blackCanvasSpecialCoin; 
+    [SerializeField] TMP_Text blackCanvasSpecialCoin;
 
+    private int emptyCoinCount = 0;
+    private int emptySpecialCoinCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        emptyCoinCount = 0;
+        emptySpecialCoinCount = 0;
+
         coinCount = 0;
         specialCoinCount = 0;
     }
 
     private void Update()
     {
+        Counter();
+        blackCanvas();
+    }
+
+    // Counter mag niet gezien worden tijdens het zwarte scherm
+    void Counter()
+    {
         textCoin.text = $"{coinCount}";
         textSpecialCoin.text = $"{specialCoinCount}";
-
-        blackCanvasCoin.text = $"{coinCount}/5";
-        blackCanvasSpecialCoin.text = $"{specialCoinCount}/1";
     }
+
+    // tijdens het zwarte scherm hoeft er niks te staan.
+    void blackCanvas()
+    {   
+        blackCanvasCoin.text = $"{emptyCoinCount}/5";
+        blackCanvasSpecialCoin.text = $"{emptySpecialCoinCount}/1";
+    }
+    
 }
--- GameJam2022/Assets/Scripts/UI/LevelChange.cs
+++ GameJam2022/Assets/Scripts/UI/LevelChange.cs
@@ -10,18 +10,31 @@ public class LevelChange : MonoBehaviour
     [SerializeField] private int levelToLoad;
     [SerializeField] private float fadeSeconds;
 
+    [Header("BlackCanvas Text")]
     [SerializeField] TMP_Text blackCanvasTextCoin;
     [SerializeField] TMP_Text blackCanvasTextSpecialCoin;
+    [SerializeField] Image blackCoinImg;
+    [SerializeField] Image blackSpecialCoinImg;
 
+    [Header("Canvas Text")]
+    [SerializeField] TMP_Text canvasTextCoin; 
+    [SerializeField] TMP_Text canvasTextSpecialCoin;
     [SerializeField] Image coinImg;
     [SerializeField] Image specialCoinImg;
-    
+
     private void Start()
     {
+        //black canvas coins
         blackCanvasTextCoin.enabled = false;
         blackCanvasTextSpecialCoin.enabled = false;
-        coinImg.enabled = false;
-        specialCoinImg.enabled = false; 
+        blackCoinImg.enabled = false;
+        blackSpecialCoinImg.enabled = false;
+
+        //normale canvas of the canvas in levels
+        canvasTextCoin.enabled = true;
+        canvasTextSpecialCoin.enabled = true;
+        coinImg.enabled = true;
+        specialCoinImg.enabled = true;
     }
 
     //This is a trigger to go to the next scene
@@ -39,10 +52,17 @@ public class LevelChange : MonoBehaviour
         animator.SetTrigger("FadeOut");
 
         yield return new WaitForSeconds(3);
+        //black canvas coins
         blackCanvasTextCoin.enabled = true;
         blackCanvasTextSpecialCoin.enabled = true;
-        coinImg.enabled = true;
-        specialCoinImg.enabled = true;
+        blackCoinImg.enabled = true;
+        blackSpecialCoinImg.enabled = true;
+
+        //normale canvas of the canvas in levels
+        canvasTextCoin.enabled = false;
+        canvasTextSpecialCoin.enabled = false;
+        coinImg.enabled = false;
+        specialCoinImg.enabled = false;
 
         yield return new WaitForSeconds(fadeSeconds);
         SceneManager.LoadScene(levelToLoad);
