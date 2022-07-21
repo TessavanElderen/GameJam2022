e84fec327623b0cf9c4c1ef3cba82d2126d669db
Tessa van Elderen <tessa.vanelderen@gmail.com>
2022-07-21 14:21:47 +0200

    Coin counter doet het.

--- GameJam2022/Assets/Scripts/Player/CoinCounter.cs
+++ GameJam2022/Assets/Scripts/Player/CoinCounter.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class CoinCounter : MonoBehaviour
 {
     [SerializeField] public int coinCount;
     [SerializeField] public int specialCoinCount;
+
+    [SerializeField] TMP_Text textCoin; 
+    [SerializeField] TMP_Text textSpecialCoin; 
 
     // Start is called before the first frame update
     void Start()
     {
         coinCount = 0;
         specialCoinCount = 0;
+    }
+
+    private void Update()
+    {
+        textCoin.text = $"{coinCount}";
+        textSpecialCoin.text = $"{specialCoinCount}";
     }
 }
